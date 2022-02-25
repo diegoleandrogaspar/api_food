c738de76cfc9a73dfc6cf081728a851399aa012b
Diego leandro <diegolgaspar000@gmail.com>
2022-02-25 19:48:45 -0300

    Modelando e implementando a exclusão de recursos com DELETE

--- src/main/java/com/algaworks/diegofood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/diegofood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import com.algaworks.diegofood.domain.model.Cozinha;
 import com.algaworks.diegofood.domain.repository.CozinhaRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -47,17 +48,36 @@ public class CozinhaController {
 
     @PutMapping("/{cozinhaId}")
     public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId,
-             @RequestBody Cozinha cozinha) {
-        Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+       @RequestBody Cozinha cozinha) {
+       Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
 
         if (cozinhaAtual != null) {
-            BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
-            //cozinhaAtual.setNome(cozinha.getNome());
+           BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+           //cozinhaAtual.setNome(cozinha.getNome());
 
-            cozinhaRepository.salvar(cozinhaAtual);
-            return ResponseEntity.ok(cozinhaAtual);
+          cozinhaRepository.salvar(cozinhaAtual);
+          return ResponseEntity.ok(cozinhaAtual);
         }
+
         return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId){
+       try{
+           Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+           if (cozinha != null){
+               cozinhaRepository.remover(cozinha);
+
+               return  ResponseEntity.noContent().build();
+           }
+           return ResponseEntity.notFound().build();
+
+       }catch (DataIntegrityViolationException exception){
+           return ResponseEntity.status(HttpStatus.CONFLICT).build();
+       }
+
     }
 
 }
