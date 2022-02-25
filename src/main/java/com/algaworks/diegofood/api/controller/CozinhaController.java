d849c6b2fa26346108b8317d2bca9a294512f477
Diego leandro <diegolgaspar000@gmail.com>
2022-02-25 19:26:57 -0300

    Modelando e implementando a atualizacao de recursos com PUT

--- src/main/java/com/algaworks/diegofood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/diegofood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.diegofood.api.controller;
 import com.algaworks.diegofood.api.model.CozinhasXmlWrapper;
 import com.algaworks.diegofood.domain.model.Cozinha;
 import com.algaworks.diegofood.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -42,6 +43,21 @@ public class CozinhaController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cozinha adicionar(@RequestBody Cozinha cozinha){
         return cozinhaRepository.salvar(cozinha);
+    }
+
+    @PutMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId,
+             @RequestBody Cozinha cozinha) {
+        Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+
+        if (cozinhaAtual != null) {
+            BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+            //cozinhaAtual.setNome(cozinha.getNome());
+
+            cozinhaRepository.salvar(cozinhaAtual);
+            return ResponseEntity.ok(cozinhaAtual);
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
