ec0a0074d2b37aca4b938d65f6232c700c5c312a
diegoleandrogaspar <diegolgaspar000@gmail.com>
2023-12-11 19:13:53 -0300

    add after Migrate

--- src/main/java/com/diegoleandro/api/domain/model/FormaPagamento.java
+++ src/main/java/com/diegoleandro/api/domain/model/FormaPagamento.java
@@ -1,16 +1,14 @@
 package com.diegoleandro.api.domain.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
+@Table(name = "forma_pagamento")
 public class FormaPagamento {
 
     @EqualsAndHashCode.Include
@@ -20,4 +18,20 @@ public class FormaPagamento {
 
     @Column(nullable = false)
     private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
