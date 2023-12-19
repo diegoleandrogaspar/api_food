853cebbc1cead0bf0dc80272db1c7a2c9deb5b65
diegoleandrogaspar <diegolgaspar000@gmail.com>
2023-12-18 21:40:09 -0300

    add Problem Details for HTTP APIs

--- src/main/java/com/diegoleandro/api/controller/CidadeController.java
+++ src/main/java/com/diegoleandro/api/controller/CidadeController.java
@@ -7,7 +7,7 @@ import com.diegoleandro.api.domain.exception.NegocioException;
 import com.diegoleandro.api.domain.model.Cidade;
 import com.diegoleandro.api.domain.repository.CidadeRepository;
 import com.diegoleandro.api.domain.service.CadastroCidadeService;
-import com.diegoleandro.api.exceptionhandler.Problema;
+import com.diegoleandro.api.exceptionhandler.Problem;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
