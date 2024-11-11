6e342fba0db03e680d98823b5eeb16c2af41b908
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-11-10 21:04:07 -0300

    conversao de entidades usando modelMapper

--- src/main/java/com/diegoleandro/api/assembler/ProdutoConverter.java
+++ src/main/java/com/diegoleandro/api/assembler/ProdutoConverter.java
@@ -1,7 +1,40 @@
 package com.diegoleandro.api.assembler;
 
+import com.diegoleandro.api.model.ProdutoDTO;
+import com.diegoleandro.api.model.input.ProdutoInput;
+import com.diegoleandro.domain.model.Produto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 @Component
-public class ProdutoConverter {
+public class ProdutoConverter implements Converter<Produto, ProdutoDTO, ProdutoInput>{
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public Produto toDomainObject(ProdutoInput produtoInput) {
+        return modelMapper.map(produtoInput, Produto.class);
+    }
+
+    @Override
+    public ProdutoDTO toDto(Produto produto) {
+        return modelMapper.map(produto, ProdutoDTO.class);
+    }
+
+    @Override
+    public Collection<ProdutoDTO> toCollectionDTO(Collection<Produto> produtos) {
+        return produtos.stream()
+                .map(produto -> toDto(produto))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void copyToDomainObject(ProdutoInput produtoInput, Produto produto) {
+        modelMapper.map(produtoInput, produto);
+    }
 }
